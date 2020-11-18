5888cf39df228cd2de057107d58e67593e603671
unknown <anisbhati101@gmail.com>
2020-11-18 14:52:55 +0530

    5 th commmit

--- src/main/java/com/example/edu/controller/ServiceController.java
+++ src/main/java/com/example/edu/controller/ServiceController.java
@@ -1,22 +1,28 @@
 package com.example.edu.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.edu.model.Student;
+import com.example.edu.repository.StudentRepository;
 import com.example.edu.service.DataService;
 
 @RestController
 public class ServiceController {
 	@Autowired
 	DataService d;
+	
+	@Autowired
+	StudentRepository s;
 	
 	@PostMapping("/addStudeny")
 	public String addStudent(@RequestBody Student s) {
@@ -33,4 +39,10 @@ public class ServiceController {
 		return "service working ........";
 	}
     // this for testing purpose
+	
+	@GetMapping("/getStudent/{id}")
+	public Optional<Student> getStudetnById(@PathVariable("id") int id) {
+		Optional<Student> ss=s.findById(id);
+		return ss;
+	}
 }
